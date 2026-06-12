bb7241667802d17aa8a7ac50e57315fb6741e7d8
md-monirujjaman <mdmonirujjaman7878@gmail.com>
2026-06-12 14:55:52 +0600

    feat: add project source files

---
+++ index.js
@@ -0,0 +1,283 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+// const PORT = 5000;
+app.get("/", (req, res) => {
+  res.send("server in running");
+});
+const PORT = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+const JWKS=createRemoteJWKSet(
+   new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+ 
+)
+
+const token= async(req,res,next)=>{
+    const auth=req.headers.authorization
+    if (!auth) {
+      return res.status(401).json({message:'unauthorized'})
+    }
+    const token=auth.split(' ')[1]
+     if (!token|| token==='undefined') {
+      return res.status(401).json({message:'unauthorized'})
+    }
+    console.log(token,'xxxx');
+         try {
+            const {payload}=await jwtVerify(token,JWKS)
+          next()
+         } catch (error) {
+  return res.status(403).json({message:'forbidden'})
+         }
+}
+
+
+// const token= async(req,res,next)=>{
+//   const authHeader=req?.headers.authorization
+//   if (!authHeader) {
+//     return res.status(401).json({
+//       message:'Unauthorized'
+//     })
+//   }
+//    const tokens=authHeader.split(' ')[1]
+
+//    if (!tokens) {
+//       return res.status(401).json({
+//       message:'Unauthorized'
+//     })
+//    }
+
+//   console.log(tokens,'hhhhhhhhhh');
+
+
+//   next()
+// }
+
+
+async function run() {
+  try {
+    // await client.connect();
+
+    const db = client.db("studyNook");
+    const studyCollection = db.collection("study");
+    const bookingCollection = db.collection("booking");
+
+    app.get("/study", async (req, res) => {
+      const result = await studyCollection.find().toArray();
+      res.json(result);
+    });
+
+    app.get('/featured',async (req,res)=>{
+      const result =await studyCollection.find().limit(3).toArray()
+      res.json(result)
+    })
+
+    app.get("/booking/:userId", async (req, res) => {
+      const { userId } = req.params;
+
+      const result = await bookingCollection.find({ userId }).toArray();
+
+      res.json(result);
+    });
+
+
+    app.get("/my-listings", async (req, res) => {
+  try {
+    const email = req.query.email;
+
+    if (!email) {
+      return res.status(400).send({ message: "Email required" });
+    }
+
+    const result = await studyCollection
+      .find({ ownerEmail: email })
+      .toArray();
+
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ message: "Server error" });
+  }
+});
+
+    app.get("/booking", async (req, res) => {
+      const result = await bookingCollection.find().toArray();
+
+      res.send(result);
+    });
+
+
+    //jwt
+
+    app.get("/study/:id",token, async (req, res) => {
+    const { id } = req.params;
+
+    const result = await studyCollection.findOne({
+      _id: new ObjectId(id),
+    });
+
+    res.send(result);
+  }
+);
+
+
+app.patch('/study/:id', async(req,res)=>{
+  const{id}=req.params
+  const updateData=req.body
+console.log(updateData);
+  const result=await studyCollection.updateOne(
+    {_id:new ObjectId(id)},
+    {$set:updateData}
+  )
+  res.json(result)
+})
+
+app.delete('/study/:id',async(req,res)=>{
+  const {id}=req.params
+  const result=await studyCollection.deleteOne({_id: new ObjectId(id)})
+  res.json(result)
+})
+
+
+// listing er jonno post
+
+app.post("/study", async (req, res) => {
+  try {
+   const newRoom = {
+  roomName: req.body.roomName,
+  image: req.body.image,
+  capacity: req.body.capacity,
+  hourlyRate: req.body.hourlyRate,
+  floor: req.body.floor,
+  description: req.body.description,
+  amenities: req.body.amenities,
+  ownerEmail: req.body.ownerEmail,
+};
+
+    // DB insert
+    const result = await studyCollection.insertOne(newRoom);
+
+    res.status(201).send({
+      success: true,
+      message: "Room created successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).send({ error: "Server error" });
+  }
+});
+
+
+
+app.patch('/booking/:id', async (req, res) => {
+
+  const { id } = req.params;
+
+  const result = await bookingCollection.updateOne(
+    {
+      _id: new ObjectId(id),
+    },
+    {
+      $set: {
+        status: "cancelled",
+      },
+    }
+  );
+
+  res.send(result);
+});
+
+
+app.post("/booking", async (req, res) => {
+  try {
+    const { roomName, date, slot } = req.body;
+
+    // 🔥 CHECK DUPLICATE BOOKING
+    const existingBooking = await bookingCollection.findOne({
+      roomName,
+      date,
+      slot,
+    });
+
+    if (existingBooking) {
+      return res.status(400).send({
+        success: false,
+        message: "This room is already booked for this time slot!",
+      });
+    }
+
+    // CREATE BOOKING
+    const result = await bookingCollection.insertOne({
+      ...req.body,
+      createdAt: new Date(),
+    });
+
+    res.status(201).send({
+      success: true,
+      message: "Booking successful",
+      data: result,
+    });
+
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Server error",
+    });
+  }
+});
+
+   
+
+//     app.post("/booking", async (req, res) => {
+//   const booking = req.body;
+
+//   const result = await bookingCollection.insertOne({
+//     ...booking,
+//     createdAt: new Date(),
+//   });
+
+//   res.json(result);
+// });
+
+    app.post("/study", token, async (req, res) => {
+      const study = req.body;
+
+      console.log(study);
+      const data = await studyCollection.insertOne(study);
+      res.json(data);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
